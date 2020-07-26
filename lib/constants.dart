59c759197ab5934c880c3fc65d4373f937835e5a
mshaileshr@gmail.com <Yes@383840>
2020-07-27 01:41:12 +0530

    loving nest.js ❤️️

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,4 +1,16 @@
 import 'package:flutter/material.dart';
 
+// Url constants
+const endpoint = "";
+const loginAPI = "";
+const registrationAPI = "";
+const productCategoriesAPI = "";
+const productSubCategoriesAPI = "";
+const productDetailAPI = "";
+const productCartAPI = "";
+const productOrderAPI = "";
+const productPaymentAPI = "";
+
+
 const kPrimaryColor = Color(0xFF6F35A5);
 const kPrimaryLightColor = Color(0xFFF1E6FF);
